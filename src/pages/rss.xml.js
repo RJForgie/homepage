578a59fa731c77792419d0857b5c7d98460d0329
Ryan Forgie <ryan.j.forgie@gmail.com>
2024-05-01 21:43:57 +1200

    only publish articles to rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('posts');
+	const articles = await getCollection('articles');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.contentType}/${post.slug}/`,
+		items: articles.map((article) => ({
+			...article.data,
+			link: `/${article.contentType}/${article.slug}/`,
 		})),
 	});
 }
