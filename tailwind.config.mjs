745424e27a48f2f3db970ea9fefdd03a02bd4940
Ryan Forgie <ryan.j.forgie@gmail.com>
2024-05-01 21:44:40 +1200

    override tailwind prose styles for h2 and links

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      typography: {
+        lg: {
+          css: {
+            h2: {
+              "font-weight": 500,
+              "font-size": "18px",
+            },
+            a: {
+              "text-decoration": "underline",
+            }
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
