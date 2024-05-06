5b88108f65f32ff8cbf757e42c28d23d3288b2ae
Ryan Forgie <ryan.j.forgie@gmail.com>
2024-05-06 21:54:18 +1200

    formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,45 +1,45 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const articles = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		contentType: z.string(),
-		highlight: z.boolean().optional()
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    contentType: z.string(),
+    highlight: z.boolean().optional(),
+  }),
 });
 
 const lists = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		contentType: z.string(),
-		highlight: z.boolean().optional()
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    contentType: z.string(),
+    highlight: z.boolean().optional(),
+  }),
 });
 
 const journal = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		contentType: z.string(),
-		highlight: z.boolean().optional()
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    contentType: z.string(),
+    highlight: z.boolean().optional(),
+  }),
 });
 
 export const collections = { articles, lists, journal };
