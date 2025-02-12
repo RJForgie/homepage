aca0bc7d55c049de5a9ddfeed2c0d025a13e5717
Ryan Forgie <ryan.j.forgie@gmail.com>
2025-02-12 21:55:09 +1300

    refactor to use astro 5 content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,45 +1,21 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z} from 'astro:content';
 
-const articles = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    contentType: z.string(),
-    highlight: z.boolean().optional(),
-  }),
+const commonSchema = () => z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+  contentType: z.string(),
+  highlight: z.boolean().optional(),
 });
 
-const lists = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    contentType: z.string(),
-    highlight: z.boolean().optional(),
+export const collections = {
+  lists: defineCollection({ 
+    type: 'content',
+    schema: commonSchema 
   }),
-});
-
-const journal = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    contentType: z.string(),
-    highlight: z.boolean().optional(),
+  journal: defineCollection({ 
+    type: 'content',
+    schema: commonSchema 
   }),
-});
-
-export const collections = { articles, lists, journal };
+};
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,17 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const articles = await getCollection('articles');
+	const journalEntries = await getCollection('journal', ({ data }) => {
+		return data.contentType === 'journal';
+	});
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: articles.map((article) => ({
-			...article.data,
-			link: `/${article.contentType}/${article.slug}/`,
+		items: journalEntries.map((journalEntry) => ({
+			...journalEntry.data,
+			link: `/${journalEntry.contentType}/${journalEntry.slug}/`,
 		})),
 	});
 }
