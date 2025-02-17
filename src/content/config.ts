2c8201ca2cfedadaeab96bf3a18f70d254304ad9
Ryan Forgie <ryan.j.forgie@gmail.com>
2025-02-17 21:04:30 +1300

    refactor out concept of contenttype

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,21 +5,17 @@ const commonSchema = () => z.object({
   description: z.string(),
   pubDate: z.coerce.date(),
   updatedDate: z.coerce.date().optional(),
-  contentType: z.string(),
   highlight: z.boolean().optional(),
 });
 
 export const collections = {
   lists: defineCollection({ 
-    type: 'content',
     schema: commonSchema 
   }),
   journal: defineCollection({ 
-    type: 'content',
     schema: commonSchema 
   }),
   links: defineCollection({ 
-    type: 'content',
     schema: commonSchema 
   }),
 };
